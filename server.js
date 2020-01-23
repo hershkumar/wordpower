a08a337f5ab4bc1cd8e1252c785646527086ef1d
Hersh Kumar <hershkumar13@gmail.com>
2020-01-22 20:05:25 -0500

    use sqlite3 instead of mongo

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-var fs = require('fs');
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var sqlite3 = require('sqlite3').verbose();
-
-// replace the uri string with your connection string.
-const pass = fs.readFileSync('pass.txt');
 
 // initialize sqlite3 database
 console.log('Initializing rankings database...');
 var db = new sqlite3.Database('./db/rankings.db');
 console.log('...done.');
 // mock adding things to the table
-db.run('CREATE TABLE IF NOT EXISTS players (name TEXT, elo INT)');
+//db.run('CREATE TABLE If NOT EXISTS players (name TEXT, elo INTEGER)');
 var stmt = db.prepare('INSERT INTO players (name, elo) VALUES(?,?)');
-stmt.run('test', 500);
+//stmt.run('Hersh', 500);
+//stmt.run('Emmy', 500);
+//stmt.run('Nate', 500);
+//stmt.run('Shawn', 5000);
 
 
 
-names = [];
+
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -38,12 +37,12 @@ http.listen(port,'0.0.0.0', function(){
     console.log('listening on *:', port);
 });
 
-let sql = `SELECT DISTINCT Name name FROM players ORDER BY name`;
+let sql = `SELECT * from players ORDER BY elo DESC`;
 app.route('/rankings').get(function(req,res){
-	db.all(sql,[],(err,rows) => {
-		rows.forEach((row) => {
-			console.log(row.name);
-		});
-
+	res.sendFile(__dirname + '/rankings.html');
+	// accessing the database
+	db.each(sql,[],(err,rows) => {
+		if (err) throw err;
+		console.log(rows.name, rows.elo)
 	});
 });
